2de22cebf315bed72e1392ba9936bfd949c794c4
makri molka <166256355+molka-makri@users.noreply.github.com>
2025-07-23 07:37:35 +0100

    Update connection.cpp

--- connection.cpp
+++ connection.cpp
@@ -9,8 +9,8 @@ bool Connection::createconnect()
 {bool test=false;
     QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
     db.setDatabaseName("Source_Projet2A");//inserer le nom de la source de données
-    db.setUserName("Molka");//inserer nom de l'utilisateur
-    db.setPassword("esprit17");//inserer mot de passe de cet utilisateur
+    db.setUserName("");//inserer nom de l'utilisateur
+    db.setPassword("");//inserer mot de passe de cet utilisateur
 
     if (db.open())
         test=true;
